86dc95f3499cdbac6355379f536fdc3893ed832f
smartlz <1009098051@qq.com>
2019-05-30 09:14:39 +0800

    美多后台项目-频道管理(指定频道获取|修改|删除)

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/channels.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/channels.py
@@ -20,6 +20,9 @@ class ChannelViewSet(ModelViewSet):
 
     # GET /meiduo_admin/goods/channels/ -> list
     # POST /meiduo_admin/goods/channels/ -> create
+    # GET /meiduo_admin/goods/channels/(?P<pk>\d+)/ -> retrieve
+    # PUT /meiduo_admin/goods/channels/(?P<pk>\d+)/ -> update
+    # DELETE /meiduo_admin/goods/channels/(?P<pk>\d+)/ -> destory
 
     # def list(self, request):
     #     qs = self.get_queryset()
@@ -32,6 +35,23 @@ class ChannelViewSet(ModelViewSet):
     #
     #     serializer.save() # -> create
     #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    # def retrieve(self, request, *args, **kwargs):
+    #     instance = self.get_object()
+    #     serializer = self.get_serializer(instance)
+    #     return Response(serializer.data)
+
+    # def update(self, request, *args, **kwargs):
+    #     instance = self.get_object()
+    #     serializer = self.get_serializer(instance, data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save() # update
+    #     return Response(serializer.data)
+
+    # def destroy(self, request, *args, **kwargs):
+    #     instance = self.get_object()
+    #     instance.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 # GET /meiduo_admin/goods/channel_types/
