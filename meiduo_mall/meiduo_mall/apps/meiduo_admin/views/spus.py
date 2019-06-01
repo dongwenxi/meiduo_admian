df69856f5306013f45ed30041f8ecd8b2e99cc57
smartlz <1009098051@qq.com>
2019-06-01 09:47:11 +0800

    美多后台项目-SKU管理(SPU规格选项数据的获取-代码优化)

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/spus.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/spus.py
@@ -1,4 +1,5 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, GenericAPIView
+from rest_framework.mixins import ListModelMixin
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -24,18 +25,36 @@ class SPUSimpleView(ListAPIView):
 
 
 # GET /meiduo_admin/goods/(?P<pk>\d+)/specs/
-class SPUSpecView(APIView):
+# class SPUSpecView(ListModelMixin, GenericAPIView):
+class SPUSpecView(ListAPIView):
     permission_classes = [IsAdminUser]
+    # 指定序列化器类
+    serializer_class = SPUSpecSerializer
+    # 指定视图所使用的查询集
+    # queryset = SPUSpecification.objects.filter(spu_id=pk)
 
-    def get(self, request, pk):
-        """
-        获取SPU规格选项数据:
-        1. 获取和spu对象关联的规格数据
-        2. 将获取规格数据序列化并返回
-        """
-        # 1. 获取和spu对象关联的规格数据
-        specs = SPUSpecification.objects.filter(spu_id=pk)
+    def get_queryset(self):
+        # 获取从url地址中提取的pk参数
+        pk = self.kwargs['pk']
 
-        # 2. 将获取规格数据序列化并返回
-        serializer = SPUSpecSerializer(specs, many=True)
-        return Response(serializer.data)
+        return SPUSpecification.objects.filter(spu_id=pk)
+
+    # 注：关闭分类
+    pagination_class = None
+
+    # def get(self, request, pk):
+    #     return self.list(request, pk)
+
+    # def get(self, request, pk):
+    #     """
+    #     self.kwargs: 字典，保存从url地址中提取的所有命名参数
+    #     获取SPU规格选项数据:
+    #     1. 获取和spu对象关联的规格数据
+    #     2. 将获取规格数据序列化并返回
+    #     """
+    #     # 1. 获取和spu对象关联的规格数据
+    #     specs = self.get_queryset()
+    #
+    #     # 2. 将获取规格数据序列化并返回
+    #     serializer = self.get_serializer(specs, many=True)
+    #     return Response(serializer.data)
